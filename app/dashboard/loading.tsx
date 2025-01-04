28fb5698c5c2fc3717aa70b7007b166d9958c6ed
Monika Garg <devmonika4302@gmail.com>
2025-01-03 20:48:17 -0500

    Add initial dashboard layout and pages with navigation links

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,5 @@
+import DashboardSkeleton from "@/app/ui/skeletons";
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <DashboardSkeleton />;
+}
